06ff217a683e539ffa80c48b56e8526034e77213
eponcecode <eponcedeleonrosas@gmail.com>
2022-07-29 10:22:16 -0500

    testing

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ export default function DontFound (){
   <tr>
     <td>    <div className='center'>
     <h3>¡Oh no! <br/>¿Cómo llegaste aquí? </h3>
-    <span>Haz clic <Link to="/">en este enlace</Link> para regresar a la página principal y retornar a la lectura!</span>
+    <span>¡Haz clic <Link to="/">en este enlace</Link> para regresar a la página principal y retornar a la lectura!</span>
   </div></td>
   </tr>
   </tbody>
